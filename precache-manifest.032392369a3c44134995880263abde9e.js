045b76f09994634c70ff00e9f464522622afd1e4
Paresh Barge <89344241+Paresh15@users.noreply.github.com>
2023-01-28 13:52:09 +0530

    Updates

---
+++ precache-manifest.032392369a3c44134995880263abde9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "920dff1163a05db6f904",
+    "url": "/TextUtils/static/css/main.46dde4f7.chunk.css"
+  },
+  {
+    "revision": "920dff1163a05db6f904",
+    "url": "/TextUtils/static/js/main.920dff11.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "8700358ec83f3ef4526f2d9e6b2a0a57",
+    "url": "/TextUtils/index.html"
+  }
+];
